e51a320a1e2df5b41c8c68a00e8645a9cc11aa28
shubhamverma <sham.shubham19@gmail.com>
2024-09-04 23:13:22 +0530

    Cors changes

--- src/app.js
+++ src/app.js
@@ -11,13 +11,7 @@ const routes = require("./routes");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3001",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: `Server is up and running` });
@@ -29,7 +23,7 @@ app.get("/health", (req, res) => {
 
 app.use("/api/v1", routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server Running on PORT: ${PORT}`);
 });
